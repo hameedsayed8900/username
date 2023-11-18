4b16dedd8e8a1e6da2cd25fc4daeb35b558d4e97
hameedsayed8900 <98809294+hameedsayed8900@users.noreply.github.com>
2023-11-18 20:18:45 +0000

    Add files via upload

---
+++ webapp.py
@@ -0,0 +1,13 @@
+import streamlit as st
+# add a module to run external python scripts
+import os
+
+
+st.title("Maigret web version")
+nicknameInput = st.text_input("Enter nickame", "…")
+if(st.button("Start")):
+    nickname = nicknameInput.title()
+    # Run the script Maigret.py with the parameter read from the nicknameInput field
+    result=os.popen("/home/user/maigret/maigret/maigret.py "+nickname).read()
+    # Print the result (this may take a few minutes)
+    st.text(result)
