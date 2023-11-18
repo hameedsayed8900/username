7216f6e5207aab16677d1df0c11753a449155fd0
hameedsayed8900 <98809294+hameedsayed8900@users.noreply.github.com>
2023-11-18 21:37:25 +0000

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,17 +1,21 @@
 import streamlit as st
 import os
 
-# Clone the repository
-os.system("git clone https://github.com/hameedsayed8900/username.git")
-
-# Set the working directory to the cloned repository
-os.chdir("username")
-
 st.title("Maigret web version")
+
 nicknameInput = st.text_input("Enter nickname", "…")
-if(st.button("Start")):
+
+if st.button("Start"):
     nickname = nicknameInput.title()
+    
+    # Specify the path to your Maigret.py script in the GitHub repository
+    script_path = "https://raw.githubusercontent.com/hameedsayed8900/username/main/Maigret.py"
+    
+    # Clone the repository to a temporary directory
+    os.system(f"git clone https://github.com/hameedsayed8900/username.git /tmp/maigret_repo")
+    
     # Run the script Maigret.py with the parameter read from the nicknameInput field
-    result = os.popen("maigret.py " + nickname).read()
+    result = os.popen(f"python /tmp/maigret_repo/Maigret.py {nickname}").read()
+    
     # Print the result (this may take a few minutes)
     st.text(result)
