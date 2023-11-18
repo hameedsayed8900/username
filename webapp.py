c473650b5c434d2185630c1bade41c84cb236a23
hameedsayed8900 <98809294+hameedsayed8900@users.noreply.github.com>
2023-11-18 21:19:01 +0000

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,13 +1,17 @@
 import streamlit as st
-# add a module to run external python scripts
 import os
 
+# Clone the repository
+os.system("git clone https://github.com/hameedsayed8900/username.git")
+
+# Set the working directory to the cloned repository
+os.chdir("your-repo")
 
 st.title("Maigret web version")
-nicknameInput = st.text_input("Enter nickame", "…")
+nicknameInput = st.text_input("Enter nickname", "…")
 if(st.button("Start")):
     nickname = nicknameInput.title()
     # Run the script Maigret.py with the parameter read from the nicknameInput field
-    result=os.popen("github.com/hameedsayed8900/username/blob/main/maigret.py "+nickname).read()
+    result = os.popen("maigret.py " + nickname).read()
     # Print the result (this may take a few minutes)
     st.text(result)
