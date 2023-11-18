8f7ab876147f86183750dee7b84d54e7992ef955
hameedsayed8900 <98809294+hameedsayed8900@users.noreply.github.com>
2023-11-18 20:54:04 +0000

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,6 +8,6 @@ nicknameInput = st.text_input("Enter nickame", "…")
 if(st.button("Start")):
     nickname = nicknameInput.title()
     # Run the script Maigret.py with the parameter read from the nicknameInput field
-    result=os.popen("maigret.py "+nickname).read()
+    result=os.popen("hameedsayed8900/username/maigret.py "+nickname).read()
     # Print the result (this may take a few minutes)
     st.text(result)
