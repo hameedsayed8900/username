53a60a91e5dc57aa8a65e7124894281355837312
hameedsayed8900 <98809294+hameedsayed8900@users.noreply.github.com>
2023-11-19 01:18:14 +0000

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import os
-import sys
 
 st.title("Maigret web version")
 
@@ -18,13 +17,10 @@ if st.button("Start"):
     # Dynamically set the script path based on the current working directory
     script_path = os.path.join(os.getcwd(), script_name)
 
-    # Add the directory containing the cloned repository to the Python path
-    sys.path.append(os.getcwd())
-
-    # Set execute permissions on the Maigret.py script
+    # Set execute permissions on the maigret.py script (if necessary)
     os.system(f"chmod +x {script_path}")
 
-    # Import main from the maigret module
+    # Import main from maigret.py
     from maigret import main
 
     # Run the main function with the parameter read from the nicknameInput field
