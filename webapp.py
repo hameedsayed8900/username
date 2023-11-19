ba8df8e4938f7dd2c525192edc3e8b5f32852421
hameedsayed8900 <98809294+hameedsayed8900@users.noreply.github.com>
2023-11-19 00:38:15 +0000

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -3,19 +3,22 @@ import os
 
 st.title("Maigret web version")
 
-nicknameInput = st.text_input("Enter nickname", "…")
+nicknameInput = st.text_input("Enter nickname", "...")
 
 if st.button("Start"):
     nickname = nicknameInput.title()
-    
+
     # Specify the path to your Maigret.py script in the GitHub repository
-    script_path = "https://raw.githubusercontent.com/hameedsayed8900/username/main/maigret.py"
-    
+    script_path = "/tmp/maigret_repo/maigret.py"
+
     # Clone the repository to a temporary directory
     os.system(f"git clone https://github.com/hameedsayed8900/username.git /tmp/maigret_repo")
-    
+
+    # Set execute permissions on the Maigret.py script
+    os.system(f"chmod +x {script_path}")
+
     # Run the script Maigret.py with the parameter read from the nicknameInput field
-    result = os.popen(f" /tmp/maigret_repo/maigret.py {nickname}").read()
-    
+    result = os.popen(f"{script_path} {nickname}").read()
+
     # Print the result (this may take a few minutes)
     st.text(result)
